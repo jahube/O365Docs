8b124fc632833bcd825adaaff576db332c42c520
jahube <70081790+jahube@users.noreply.github.com>
2020-09-08 03:28:46 +0200

    Update OWA.Folderpermissions.ps1

--- Folderpermissions/OWA.Folderpermissions.ps1
+++ Folderpermissions/OWA.Folderpermissions.ps1
@@ -1,26 +1,28 @@
-﻿# connect extended
+﻿# Folder permissions in OWA
+#-------------------------
 
-$admin = ""
+$access = 'Reviewer'
 
-#Admin + PS check
-Try { Set-ExecutionPolicy RemoteSigned -Force -EA stop } catch { Write-host "restart as Admin / check Permissions" -F Yellow }
-if ($($PSVersionTable.PSVersion.ToString()) -lt "5") { Write-Host "Powerhell $($PSVersionTable.PSVersion.ToString()) doesn't support Install-module" -F yellow}
+$MBX = "mailbox@to.share" # please modify
 
-#admin
-if (!($admin)) {$admin = read-host -Prompt "Enter Global Admin [Admin@domain.com]" }
-#credential
-if (!($cred)) {$cred = Get-Credential $admin}
+$user = "user@gets.access" # please modify
 
-#Trust Powershell Gallery
-if (!(get-PackageProvider nuget)) { write-host "Adding NUGet Package Provider" -F green; Install-PackageProvider -Name NuGet -Force -Scope CurrentUser -Confirm:$false }
-if ($((Get-PSRepository -Name "PSGallery").InstallationPolicy) -ne 'trusted') { write-host "Adding PSGallery Trust" -F green;Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted }
+$inbox = "Inbox"
 
-#MSOL Check / Login - Else TLS + install
-if(Get-MsolDomain -ErrorAction SilentlyContinue) { Write-host 'MSOL connected' -F green} else { try { Connect-MsolService -Credential $cred } catch { 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-try {Install-Module MSOnline -Scope CurrentUser -Confirm:$false -EA stop } catch { Write-host $Error[0].Exception.Message -F yellow }
-try { Connect-MsolService -Credential $cred -EA stop } catch { Write-host $Error[0].Exception.Message -F yellow }}}
+$customfolder = "Posteingang"
 
-# check EXO V2 Installed + Connect Exo
-if (!(get-module ExchangeOnlineManagement)) { Try { Install-Module ExchangeOnlineManagement -Scope CurrentUser -Confirm:$false -EA stop } catch { Write-host $Error[0].Exception.Message -F yellow}}
-Try{ Connect-ExchangeOnline -Credential $cred -EA stop } catch { Connect-ExchangeOnline -UserPrincipalName $admin }
+# Add Root Permission - REQUIRED
+Try { Add-MailboxFolderPermission -identity $MBX -User $user -Accessrights Reviewer -EA stop } catch { write-host $Error[0].Exception.message -F yellow }
+
+# Add Inbox Permission
+Try { Add-MailboxFolderPermission -identity $($MBX+':\'+$inbox) -User $user -Accessrights Reviewer -EA stop } catch { write-host $Error[0].Exception.message -F yellow }
+
+# Add customfolder Permission
+Try { Add-MailboxFolderPermission -identity $($MBX+':\'+$customfolder) -User $user -Accessrights Reviewer -EA stop } catch { write-host $Error[0].Exception.message -F yellow }
+
+
+# Add Permission to all foldertypes below
+$types = "Inbox","Outbox","SentItems","Drafts","JunkEmail","Archive","Contacts","Calendar","Notes","QuickContacts","RecipientCache","User Created"
+
+foreach($F in (Get-MailboxFolderStatistics $MBX | ? {$_.foldertype.tostring() -in ($types)})){ $FN = $MBX + ':' + $F.FolderPath.Replace('/','\');
+Try { Add-MailboxFolderPermission $FN -User $user -AccessRights $access -EA stop } catch { write-host $Error[0].Exception.message -F yellow }}
